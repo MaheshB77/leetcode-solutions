d93979b920e6b1703d382e0b3d70a2c296fde915
Mahesh Bansode <47536121+MaheshB77@users.noreply.github.com>
2024-02-07 01:46:24 +0530

    Time: 1 ms (99.76%) | Memory: 48.5 MB (41.89%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,10 +11,43 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return brutForce(headA, headB);
+        // return brutForce(headA, headB);
+        return solution1(headA, headB);
     }
 
-    
+    /**
+        Time -> O(m) + O(n) + O(m | n)
+        Spacec -> O(1)
+     */
+    public ListNode solution1(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int l1 = 0;
+        int l2 = 0;
+        while (temp1 != null) {
+            l1++;
+            temp1 = temp1.next;
+        }
+        while (temp2 != null) {
+            l2++;
+            temp2 = temp2.next;
+        }
+
+        if (l1 < l2) {
+            return getIntersection(headA, headB, l2 - l1);
+        } else {
+            return getIntersection(headB, headA, l1 - l2);
+        }
+    }
+
+    /**
+        Time -> O(m) + O(n)
+        Space -> O(n)
+     */
     public ListNode brutForce(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
             return null;
@@ -34,5 +67,18 @@ public class Solution {
             temp2 = temp2.next;
         }
         return null;
+    }
+
+    // Util methods
+    public ListNode getIntersection(ListNode n1, ListNode n2, int diff) {
+        while (diff != 0) {
+            diff--;
+            n2 = n2.next;
+        }
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
     }
 }
