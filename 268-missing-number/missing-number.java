6425d3c4d0ee74a17cd10a48fd40065bcf45939b
Mahesh Bansode <47536121+MaheshB77@users.noreply.github.com>
2024-02-25 23:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (90.45%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,6 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        return brutForce(nums);
+        // return brutForce(nums);
+        return solution(nums);
+    }
+
+    public int solution(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        for(int num : nums) sum+=num;
+        return (n * (n + 1))/2 - sum;
     }
 
     /**
