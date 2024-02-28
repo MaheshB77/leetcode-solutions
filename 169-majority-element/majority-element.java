3712bb204a4c8dbd4ba7549dcda40d9d9121806f
Mahesh Bansode <47536121+MaheshB77@users.noreply.github.com>
2024-02-29 00:22:51 +0530

    Time: 2 ms (77.40%) | Memory: 52.8 MB (7.47%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,34 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return brutForce(nums);
+        // return brutForce(nums);
+        return optimalSolution(nums);
     }
 
+    /**
+        Boyer Moore Algorithm
+        Time -> O(n)
+        Space -> O(1)
+     */
+    public int optimalSolution(int[] nums) {
+        int result = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+            }
+            if (result != num) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+        return result;
+    }
+
+    /**
+        Time -> O(n)
+        Space -> O(n)
+     */
     public int brutForce(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
